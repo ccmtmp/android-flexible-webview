dedeb7cf7e2c2cfdcd78fe8ba97307fc1c511e49
Ryan Cheung <cheungcmtmp@gmail.com>
2020-08-16 14:37:18 +0800

    Init project

---
+++ app/src/main/java/com/ccm/lib/flexiblewebview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ccm.lib.flexiblewebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
